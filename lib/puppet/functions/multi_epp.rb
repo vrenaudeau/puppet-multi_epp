ba99fb7ffa767ec28a26b25d0d150f0d874d22a0
vrenaudeau <40024898+vrenaudeau@users.noreply.github.com>
2018-06-07 12:14:23 +0200

    Update multi_epp.rb

--- lib/puppet/functions/multi_epp.rb
+++ lib/puppet/functions/multi_epp.rb
@@ -1,41 +1,23 @@
 #Puppet::Functions.create_function(:multi_epp, Puppet::Functions::InternalFunction) do
 module Puppet::Parser::Functions
 newfunction(:multi_epp, :type => :rvalue) do | args |
+  
+  templates = args[0]
+  datas = args[1]
+  env_name = self.compiler.environment
+  
+  templates.each do |template_file|
+    if template_path = Puppet::Parser::Files.find_template(template_file, env_name)
+      contents = call_function('epp', template_path, params)
 
-  dispatch :no_param do
-  end
-
-  dispatch :epp_templates do
-    scope_param
-    param 'Array[String]', :templates
-    optional_param 'Hash[Pattern[/^\w+$/], Any]', :parameters
-  end
-
-
-  def no_param
-    raise ArgumentError, "#{self.class.name}(): must be provided at least one epp template"
-  end
-
-  def epp_templates(scope, templates, params = {})
-
-    template_path = nil
-    contents = nil
-    env_name = scope.compiler.environment
-
-    templates.each do |template_file|
-      if template_path = Puppet::Parser::Files.find_template(template_file, env_name)
-        contents = call_function('epp', template_path, params)
-
-        break # exit the loop as soon as we match a file
-      end
+      break # exit the loop as soon as we match a file
     end
-
-    if contents.nil?
-      files = templates.join(', ')
-      raise ArgumentError, "#{self.class.name}(): No match found for #{files}"
-    end
-
-    contents
   end
 
+  if contents.nil?
+    files = templates.join(', ')
+    raise ArgumentError, "#{self.class.name}(): No match found for #{files}"
+  end
+
+  contents
 end
