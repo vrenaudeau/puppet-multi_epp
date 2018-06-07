045617bacb2bca614ae697594edae98c35f76cd4
vrenaudeau <40024898+vrenaudeau@users.noreply.github.com>
2018-06-07 12:17:46 +0200

    Update multi_epp.rb

--- lib/puppet/functions/multi_epp.rb
+++ lib/puppet/functions/multi_epp.rb
@@ -1,24 +1,25 @@
 #Puppet::Functions.create_function(:multi_epp, Puppet::Functions::InternalFunction) do
 module Puppet::Parser::Functions
-require 'epp'
-newfunction(:multi_epp, :type => :rvalue) do | args |
-  
-  templates = args[0]
-  datas = args[1]
-  env_name = self.compiler.environment
-  
-  templates.each do |template_file|
-    if template_path = Puppet::Parser::Files.find_template(template_file, env_name)
-      contents = call_function('epp', template_path, params)
+  require 'epp'
+  newfunction(:multi_epp, :type => :rvalue) do | args |
+    
+    templates = args[0]
+    datas = args[1]
+    env_name = self.compiler.environment
+    
+    templates.each do |template_file|
+      if template_path = Puppet::Parser::Files.find_template(template_file, env_name)
+        contents = call_function('epp', template_path, params)
 
-      break # exit the loop as soon as we match a file
+        break # exit the loop as soon as we match a file
+      end
     end
-  end
 
-  if contents.nil?
-    files = templates.join(', ')
-    raise ArgumentError, "#{self.class.name}(): No match found for #{files}"
-  end
+    if contents.nil?
+      files = templates.join(', ')
+      raise ArgumentError, "#{self.class.name}(): No match found for #{files}"
+    end
 
-  contents
+    contents
+  end
 end
