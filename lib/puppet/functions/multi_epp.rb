1c013b05330afe1c0f7e9ec52439f6ea906715b8
vrenaudeau <40024898+vrenaudeau@users.noreply.github.com>
2018-06-08 11:34:08 +0200

    Update multi_epp.rb

--- lib/puppet/functions/multi_epp.rb
+++ lib/puppet/functions/multi_epp.rb
@@ -1,39 +1,33 @@
-Puppet::Functions.create_function(:multi_epp) do
+# encoding: UTF-8
+module Puppet::Parser::Functions
+  newfunction(:multi_epp, :type => :rvalue) do |args|
+    begin
+      templates = args[0]
+      params = args[1]
 
-  dispatch :no_param do
-  end
-
-  dispatch :epp_templates do
-    scope_param
-    param 'Array[String]', :templates
-    optional_param 'Hash[Pattern[/^\w+$/], Any]', :parameters
-  end
-
-
-  def no_param
-    raise ArgumentError, "#{self.class.name}(): must be provided at least one epp template"
-  end
-
-  def epp_templates(scope, templates, params = {})
-
-    template_path = nil
-    contents = nil
-    env_name = scope.compiler.environment
-
-    templates.each do |template_file|
-      if template_path = Puppet::Parser::Files.find_template(template_file, env_name)
-        contents = call_function('epp', template_path, params)
-
-        break # exit the loop as soon as we match a file
+      template_path = nil
+      contents    = nil
+      env_name = self.compiler.environment
+      
+      templates.each do |template_file|
+        if template_path = Puppet::Parser::Files.find_template(template_file, env_name)
+          if Integer(lookupvar('puppetversion')[0,1]) > 3
+            contents = call_function('epp', [template_path, params])
+          else
+            contents = call_function('epp', template_path, params)
+          end
+  
+          break
+        end
       end
+  
+      if contents.nil?
+        files = templates.join(', ')
+        raise ArgumentError, "#{self.class.name}(): No match found for #{files}"
+      end
+      
+      contents
     end
-
-    if contents.nil?
-      files = templates.join(', ')
-      raise ArgumentError, "#{self.class.name}(): No match found for #{files}"
-    end
-
-    contents
   end
 
 end
